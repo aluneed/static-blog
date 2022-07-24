da02d749f02b57419e3153a840cd324a55da46f6
Aluneed <31174087+aluneed@users.noreply.github.com>
2022-07-24 21:00:52 +0800

    branch for aluneed.github.io

--- src/app/tags.ts
+++ src/app/tags.ts
@@ -4,12 +4,6 @@ export interface Tag {
 }
 
 export const tags: Tag[] = [
-  {"name":"tag1","count":3},
-  {"name":"tag2","count":2},
-  {"name":"blog","count":2},
-  {"name":"hello","count":1},
-  {"name":"frontend","count":1},
-  {"name":"angular","count":1},
-  {"name":"material","count":1},
-  {"name":"typescript","count":1},
+  {"name":"blog","count":1},
+  {"name":"test","count":1},
 ]
---
+++ src/assets/posts/blog-test.md
@@ -0,0 +1,12 @@
+<!--
+id: 0
+title: blog test
+date: 2022-07-24
+tags: blog, test
+category: blog
+type: blog
+-->
+
+# blogTest
+
+![alt](images/test-picture.jpg "title")
